068f51d53ce6044e440e4e93407759f4c1a4607c
Anthony Lee <anthonylee5519@gmail.com>
2025-01-10 09:05:25 -0700

    day 1 stuff

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, "views", "index.html"))
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+  console.log(`server running on port ${PORT}`);
+  
+})
